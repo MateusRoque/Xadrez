c19ad255a510b765577242ba930a8ff5e18d7c25
Mateus Roque <mateusroque1406@gmail.com>
2024-01-26 16:18:02 -0300

    Criação do tabuleiro através do terminal do Eclipse

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,10 +1,15 @@
 package Application;
 
+import Boardgame.Position;
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ola");
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
