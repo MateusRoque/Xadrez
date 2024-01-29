bcb07afb038190a4971db4d84e63383db6ee6a75
Mateus Roque <mateusroque1406@gmail.com>
2024-01-29 13:30:13 -0300

    Criação de peças do tabuleiro

--- src/Boardgame/Board.java
+++ src/Boardgame/Board.java
@@ -15,10 +15,13 @@ public class Board {
 		return pieces[row][column];
 	}
 	
-	public Piece pice(Position position) {
+	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 	public int getRows() {
 		return rows;
 	}
